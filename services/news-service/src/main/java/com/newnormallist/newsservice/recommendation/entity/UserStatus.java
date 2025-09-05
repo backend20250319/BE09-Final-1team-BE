e133f37b551077a08c5ae720177e3e93af133df6
heechae <chaehee15@naver.com>
2025-09-05 21:36:16 +0900

    [fix] 여러 서비스 동시 빌드 테스트

--- services/news-service/src/main/java/com/newnormallist/newsservice/recommendation/entity/UserStatus.java
+++ services/news-service/src/main/java/com/newnormallist/newsservice/recommendation/entity/UserStatus.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserStatus {
-    // ACTIVE : 활성, INACTIVE : 비활성, DELETED : 탈퇴
     ACTIVE,
     INACTIVE,
     DELETED;

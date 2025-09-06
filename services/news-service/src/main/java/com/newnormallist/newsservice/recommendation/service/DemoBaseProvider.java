27b5aa885d57755199c84ad0261ef7e1d3aadbea
Berry-mas <junseo9145@gmail.com>
2025-09-06 23:36:16 +0900

    [fix] jenkinsfile 수정

--- services/news-service/src/main/java/com/newnormallist/newsservice/recommendation/service/DemoBaseProvider.java
+++ services/news-service/src/main/java/com/newnormallist/newsservice/recommendation/service/DemoBaseProvider.java
@@ -6,6 +6,7 @@ import com.newnormallist.newsservice.recommendation.entity.RecommendationCategor
 import com.newnormallist.newsservice.recommendation.entity.AgeBucket;
 
 
+// 자체 기준
 // 연령/성별에 따른 기본 분포 D(c) 제공 인터페이스.
 public interface DemoBaseProvider {
     Map<RecommendationCategory, Double> getBase(AgeBucket age, String gender); // sum=1

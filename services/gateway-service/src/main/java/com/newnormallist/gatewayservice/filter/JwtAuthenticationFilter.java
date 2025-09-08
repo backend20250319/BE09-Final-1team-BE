d562e527246826ee2f033e260332e1b5319df1f0
heechae <chaehee15@naver.com>
2025-09-08 14:43:06 +0900

    [fix] eks 연동 테스트11

--- services/gateway-service/src/main/java/com/newnormallist/gatewayservice/filter/JwtAuthenticationFilter.java
+++ services/gateway-service/src/main/java/com/newnormallist/gatewayservice/filter/JwtAuthenticationFilter.java
@@ -62,7 +62,7 @@ public class JwtAuthenticationFilter implements GlobalFilter, Ordered {
 
       log.info("✅ [Gateway] Claims extracted. UserId: {}, Role: {}", userId, role);
 
-      // 새로운 헤더를 추가하여 각 서비스로 전달
+      // 새로운 헤더를 추가하여 각 서비스로 전달 (마이크로서비스들이 이 헤더를 통해 사용자 정보 확인 가능)
       ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
               .header("X-User-Id", String.valueOf(userId))
               .header("X-User-Role", role)
